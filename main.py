034d296215ec65df8066fca808f9e5424ebe1b89
matiasfeliu92 <matumazparrote@gmail.com>
2026-01-28 14:37:08 -0300

    update main.py for run and test ELT process

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from scripts.Scrapper import Scrap
+import pandas as pd
+import os
+
+from src.scripts.scraping import Scraping
+from src.config.scraping_settings import ScrapingSettings
+
+scraping_settings = ScrapingSettings()
 
 if __name__ == "__main__":
-    scrap = Scrap()
-    scrap.extract_links()
-    scrap.extract_data()
+    products_links_dir = os.path.join(scraping_settings.INPUTS_DIR, "product_links_for_ELT_scraping.csv")
+    links = pd.read_csv(products_links_dir, sep=";")
+    for _, row in links[0:5].iterrows():
+        # if "megatone.net" not in row["link"]:
+        # if "fravega.com" not in row["link"]:
+        # if "musimundo.com" not in row["link"]:
+        # if "naldo.com.ar" not in row["link"]:
+        #     continue
+        link = row["link"]
+        scraper = Scraping(link)
+        product_data = scraper.run()
+        print(product_data)
