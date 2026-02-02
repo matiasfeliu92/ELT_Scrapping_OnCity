b21983e5e0fa282e018dc46dad9ccd25ae36b05e
matiasfeliu92 <matumazparrote@gmail.com>
2026-02-02 12:51:58 -0300

    replace prints by custom loggings

--- src/utils/extract_elements.py
+++ src/utils/extract_elements.py
@@ -1,22 +1,25 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from src.config.logger import LoggerConfig
+
 class ExtractElements:
     def __init__(self, __driver__):
         self.driver = __driver__
+        self.logger = LoggerConfig.get_logger(self.__class__.__name__)
 
     def safe_find_elements(self, by, path, multiple=False, timeout=20):
         try:
             wait = WebDriverWait(self.driver, timeout)
             wait.until(EC.visibility_of_element_located((by, path)))
             if multiple:
-                print("MULTIPLE")
+                self.logger.info("MULTIPLE")
                 elements = self.driver.find_elements(by, path)
                 return elements if elements else []
             else:
-                print("ELEMENT")
+                self.logger.info("ELEMENT")
                 element = self.driver.find_element(by, path)
                 return element if element else None
         except Exception as e:
-            print("Fallo al obtener el texto:", e)
+            self.logger.error("Fallo al obtener el texto:", str(e))
             return None
