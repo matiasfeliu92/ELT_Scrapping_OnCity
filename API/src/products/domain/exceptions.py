220f4281b76ca9a9824560a8c400cfd4b91ba70c
matiasfeliu92 <matumazparrote@gmail.com>
2026-03-09 10:10:16 -0300

    delete old product exceptions

--- API/src/products/domain/exceptions.py
+++
@@ -1,7 +0,0 @@
-class DomainError(Exception):
-    """Base class for domain-level exceptions."""
-    pass
-
-class ProductNotFoundError(DomainError):
-    """Raised when a requested product cannot be found in the repository."""
-    pass
